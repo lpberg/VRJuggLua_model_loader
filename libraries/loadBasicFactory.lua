4e003c3bcdc319fb08c2282d12ece7108d755cc3
Leif Berg <lpberg@iastate.edu>
2013-05-02 15:45:49 -0500

    adjusted factory position

--- libraries/loadBasicFactory.lua
+++ libraries/loadBasicFactory.lua
@@ -2,6 +2,7 @@ require("getScriptFilename")
 vrjLua.appendToModelSearchPath(getScriptFilename())
 
 local factory = Transform{
+	position = {0,0,5},
 	orientation = AngleAxis(Degrees(-90), Axis{1.0, 0.0, 0.0}),
 	scale = ScaleFrom.inches,
 	Model(vrjLua.findInModelSearchPath([[../models/basicfactory.ive]]))
