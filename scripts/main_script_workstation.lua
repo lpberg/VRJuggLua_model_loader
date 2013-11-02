bb0d6a4b9c21aedad0ce91b137faa46eb1dded3f
unknown <lpberg@yoda.vrac.iastate.edu>
2013-11-02 12:06:49 -0500

    refactored main workstation script added non-manip functionality

--- scripts/main_script_workstation.lua
+++ scripts/main_script_workstation.lua
@@ -1,13 +1,5 @@
-require("getScriptFilename")
-vrjLua.appendToModelSearchPath(getScriptFilename())
-
---set buttons required for SimSparta
-nextBtn = gadget.DigitalInterface("VJButton0")
-dragBtn = gadget.DigitalInterface("VJButton2")
--- prevBtn = gadget.DigitalInterface("VJButton1")
---for scaling action frame at bottom - no more buttons!
--- increaseBtn = 
--- decreaseBtn = 
+require "AddAppDirectory"
+AddAppDirectory()
 
 --load useful_tools for getRandomColor(), changeTransformColor(), and scaleTransform()
 dofile([[libraries\useful_tools.lua]])
@@ -19,57 +11,102 @@ dofile([[libraries\loadBasicFactory.lua]])
 dofile([[libraries\simpleLights.lua]])
 
 local models = {}
---set different postion for working on workstation (vs. METaL)
-local global_position = {1, 1.5, .25}
 
-local loadModelPathsFromFile = function(filename)
-	local file = assert(io.open(filename, "r"))
+local loadModelPathsFromFileAndCreateManipulatables = function(a)
+	local file = assert(io.open(a.filename, "r"))
+	local parent = a.parent or RelativeTo.World
+	local my_position = a.position = {1, 1.5, .25}
+	local my_scale = a.scale or 1
 	for v in file:lines() do
-		model = Transform{Model(v)}
-		model_xform = Transform{position = global_position, model}
-		table.insert(models, model)
-		changeTransformColor(model_xform, getRandomColor())
-		RelativeTo.World:addChild(createManipulatableObject(model_xform))
-	end
-end
-
-
-local loadOSGsAndIves = function()
-	for i, v in pairs(arg) do
-		if string.find(v, ".osg") or string.find(v, ".ive") then
+		if  string.find(v, ".txt") then
+			loadModelPathsFromFileAndCreateManipulatables(v)
+		else
 			model = Transform{Model(v)}
-			model_xform = Transform{position = global_position, model}
+			model_xform = Transform{position = my_position, model}
 			table.insert(models, model)
 			changeTransformColor(model_xform, getRandomColor())
-			RelativeTo.World:addChild(createManipulatableObject(model_xform))
-		elseif  string.find(v, ".txt") then
-			loadModelPathsFromFile(v)
+			parent:addChild(createManipulatableObject(model_xform))
 		end
 	end
 end
 
 
+local loadModelsAndCreateManipulatables = function(a)
+	for i, v in pairs(arg) do
+		if  string.find(v, ".txt") then
+			loadModelPathsFromFileAndCreateManipulatables(v)
+		else
+			local parent = a.parent or RelativeTo.World
+			local my_position = a.position = {1, 1.5, .25}
+			local my_scale = a.scale or 1
+			model = Transform{Model(v)}
+			model_xform = Transform{position = my_position, model}
+			table.insert(models, model)
+			changeTransformColor(model_xform, getRandomColor())
+			parent:addChild(createManipulatableObject(model_xform))
+		end
+	end
+end
 
-loadOSGsAndIves()
+local loadModelPathsFromFile = function(a)
+	local parent = a.parent or RelativeTo.World
+	local my_scale = a.scale or 1
+	local file = assert(io.open(a.filename, "r"))
+	for v in file:lines() do
+		model = Transform{scale = my_scale, Model(v)}
+		changeTransformColor(model, getRandomColor())
+		parent:addChild(model)
+	end
+end
 
---adding SimSparted frame action
-SimSparta(dragBtn, nextBtn, prevBtn)
 
--- add frame action for scaling objects - featured commented out as workstation has not more buttons!
--- Actions.addFrameAction(
-	-- function()
-		-- while true do
-			-- if increaseBtn.justPressed then
-				-- for _, v in ipairs(models) do
-					-- scaleTransform(v, v:getScale():x() * 1.05)
-				-- end
-			-- end
-			-- if decreaseBtn.justPressed then
-				-- for _, v in ipairs(models) do
-					-- scaleTransform(v, v:getScale():x() * 0.95)
-				-- end
-			-- end
-			-- Actions.waitForRedraw()
-		-- end
-	-- end
--- )
+local loadModels = function(a)
+	for i, v in pairs(arg) do
+		if  string.find(v, ".txt") then
+			loadModelPathsFromFile(v)
+		else
+			local parent = a.parent or RelativeTo.World
+			local my_scale = a.scale or 1
+			model = Transform{scale = my_scale, Model(v)}
+			changeTransformColor(model, getRandomColor())
+			parent:addChild(model)
+		end
+	end
+end
+
+function main()
+	loadModels()
+end
+main()
+
+local function startSimSparta()
+	nextBtn = gadget.DigitalInterface("VJButton0")
+	dragBtn = gadget.DigitalInterface("VJButton2")
+	-- prevBtn = gadget.DigitalInterface("VJButton1")
+	SimSparta(dragBtn, nextBtn, prevBtn)
+end
+-- startSimSparta()
+
+local function addScalingFrameAction()
+	-- for scaling action frame at bottom - no more buttons!
+	-- increaseBtn = 
+	-- decreaseBtn = 
+	Actions.addFrameAction(
+		function()
+			while true do
+				if increaseBtn.justPressed then
+					for _, v in ipairs(models) do
+						scaleTransform(v, v:getScale():x() * 1.05)
+					end
+				end
+				if decreaseBtn.justPressed then
+					for _, v in ipairs(models) do
+						scaleTransform(v, v:getScale():x() * 0.95)
+					end
+				end
+				Actions.waitForRedraw()
+			end
+		end
+	)
+end
+-- addScalingFrameAction()
