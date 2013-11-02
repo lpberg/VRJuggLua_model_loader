f52b551e2be5fa907d8c688701e4f3bd266666e6
Leif Berg <lpberg@iastate.edu>
2013-11-02 12:28:00 -0500

    starting to make primary scripts generic to elimate number of files - not done yet

--- scripts/main_script_workstation.lua
+++ scripts/main_script_workstation.lua
@@ -1,6 +1,32 @@
 require "AddAppDirectory"
 AddAppDirectory()
 
+-- local useMETaLSettings = true
+
+if useMETaLSettings then
+	print("VRJuggLua_Model_Loader: Loading METaL Settings")
+	dragBtn = gadget.DigitalInterface("WMButtonB")
+	nextBtn = gadget.DigitalInterface("WMButtonRight")
+	prevBtn = gadget.DigitalInterface("WMButtonLeft")
+	resetBtn = gadget.DigitalInterface("WMButton1")
+	--for scaling action frame at bottom
+	increaseBtn = gadget.DigitalInterface("WMButtonPlus")
+	decreaseBtn = gadget.DigitalInterface("WMButtonMinus")
+	global_pos = {2, 1.5, 1.5}
+else
+	print("VRJuggLua_Model_Loader: Loading Workstation Settings")
+	nextBtn = gadget.DigitalInterface("VJButton0")
+	dragBtn = gadget.DigitalInterface("VJButton2")
+	prevBtn = gadget.DigitalInterface("VJButton1")
+	-- for scaling action frame at bottom - no more buttons!
+	increaseBtn = nil
+	decreaseBtn = nil
+	global_pos = {1, 1.5, .25}
+end
+
+local function startSimSparta()
+	SimSparta(dragBtn, nextBtn, prevBtn, resetBtn)
+end
 --load useful_tools for getRandomColor(), changeTransformColor(), and scaleTransform()
 dofile([[libraries\useful_tools.lua]])
 --load SimSparta for createManipulatableObject() and SimSparta() - frame action call
@@ -15,7 +41,7 @@ local models = {}
 local loadModelPathsFromFileAndCreateManipulatables = function(a)
 	local file = assert(io.open(a.filename, "r"))
 	local parent = a.parent or RelativeTo.World
-	local my_position = a.position = {1, 1.5, .25}
+	local my_position = a.position = global_pos
 	local my_scale = a.scale or 1
 	for v in file:lines() do
 		if  string.find(v, ".txt") then
@@ -37,7 +63,7 @@ local loadModelsAndCreateManipulatables = function(a)
 			loadModelPathsFromFileAndCreateManipulatables(v)
 		else
 			local parent = a.parent or RelativeTo.World
-			local my_position = a.position = {1, 1.5, .25}
+			local my_position = a.position = global_pos
 			local my_scale = a.scale or 1
 			model = Transform{Model(v)}
 			model_xform = Transform{position = my_position, model}
@@ -46,19 +72,20 @@ local loadModelsAndCreateManipulatables = function(a)
 			parent:addChild(createManipulatableObject(model_xform))
 		end
 	end
+	startSimSparta()
 end
 
 local loadModelPathsFromFile = function(a)
 	local parent = a.parent or RelativeTo.World
 	local my_scale = a.scale or 1
+	local my_position = a.position = global_pos
 	local file = assert(io.open(a.filename, "r"))
 	for v in file:lines() do
-		model = Transform{scale = my_scale, Model(v)}
+		model = Transform{position = my_position, scale = my_scale, Model(v)}
 		changeTransformColor(model, getRandomColor())
 		parent:addChild(model)
 	end
 end
-
 
 local loadModels = function(a)
 	for i, v in pairs(arg) do
@@ -67,46 +94,42 @@ local loadModels = function(a)
 		else
 			local parent = a.parent or RelativeTo.World
 			local my_scale = a.scale or 1
-			model = Transform{scale = my_scale, Model(v)}
+			local my_position = a.position = global_pos
+			model = Transform{position = my_position, scale = my_scale, Model(v)}
 			changeTransformColor(model, getRandomColor())
 			parent:addChild(model)
 		end
 	end
 end
 
+local function addScalingFrameAction()
+	-- only gets added if buttons are defined
+	if increaseBtn ~= nil and decreaseBtn ~= nil then
+		Actions.addFrameAction(
+			function()
+				while true do
+					if increaseBtn.justPressed then
+						for _, v in ipairs(models) do
+							scaleTransform(v, v:getScale():x() * 1.05)
+						end
+					end
+					if decreaseBtn.justPressed then
+						for _, v in ipairs(models) do
+							scaleTransform(v, v:getScale():x() * 0.95)
+						end
+					end
+					Actions.waitForRedraw()
+				end
+			end
+		)
+	end
+end
+addScalingFrameAction()
+
+--TODO: pass in with launcher?
 function main()
-	loadModels()
+	loadModels({parent = nil, scale = 1, position = nil})
+	-- OR
+	-- loadModelsAndCreateManipulatables({parent = nil, position = nil, scale = 1})
 end
 main()
-
-local function startSimSparta()
-	nextBtn = gadget.DigitalInterface("VJButton0")
-	dragBtn = gadget.DigitalInterface("VJButton2")
-	-- prevBtn = gadget.DigitalInterface("VJButton1")
-	SimSparta(dragBtn, nextBtn, prevBtn)
-end
--- startSimSparta()
-
-local function addScalingFrameAction()
-	-- for scaling action frame at bottom - no more buttons!
-	-- increaseBtn = 
-	-- decreaseBtn = 
-	Actions.addFrameAction(
-		function()
-			while true do
-				if increaseBtn.justPressed then
-					for _, v in ipairs(models) do
-						scaleTransform(v, v:getScale():x() * 1.05)
-					end
-				end
-				if decreaseBtn.justPressed then
-					for _, v in ipairs(models) do
-						scaleTransform(v, v:getScale():x() * 0.95)
-					end
-				end
-				Actions.waitForRedraw()
-			end
-		end
-	)
-end
--- addScalingFrameAction()
