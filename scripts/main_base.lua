8a7185e1c828a6c6586217ccc8e0fa70e2973b6d
Leif Berg <lpberg@iastate.edu>
2014-02-05 13:04:59 -0600

    refactored main scripts to call centeral funcion in main_base

---
+++ scripts/main_base.lua
@@ -0,0 +1,146 @@
+require "AddAppDirectory"
+AddAppDirectory()
+
+--load useful_tools for getRandomColor(), changeTransformColor(), and scaleTransform()
+runfile[[../libraries/useful_tools.lua]]
+--add lighting to scene
+runfile([[../libraries/simpleLights.lua]])
+
+local function CenterTransformAtPosition(xform, pos)
+	local bound = xform:getBound()
+	return Transform{
+		position = -bound:center() + Vec(unpack(pos)),
+		xform,
+	}
+end
+
+local function isAcceptableFileType(file)
+	if string.find(file, ".txt") then
+		return true
+	elseif string.find(file, ".osg") then
+		return true
+	elseif string.find(file, ".ive") then
+		return true
+	elseif string.find(file, ".obj") then
+		return true
+	elseif string.find(file, ".3ds") then
+		return true
+	else
+		return false
+	end
+end
+
+local function trim(s)
+	return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+local function loadModel(file_name, a)
+	print("Adding: ", file_name)
+	local model = Transform{position = a.position, scale = a.scale, Model(file_name)}
+	if not a.hasColor then
+		changeTransformColor(model, getRandomColor())
+	end
+	if a.manipulate then
+		model_xform = Transform{position = a.position, model}
+		table.insert(a.models, model)
+		model = createManipulatableObject(model_xform)
+	end
+	a.parent:addChild(model)
+end
+
+local function loadModelFilesFromTxtFile(a)
+	local file = assert(io.open(a.filename, "r"))
+	for v in file:lines() do
+		v = trim(v)
+		loadModel(v, a)
+	end
+end
+
+
+local function loadIndividualModelFiles(a)
+	for i, v in pairs(arg) do
+		if isAcceptableFileType(v) then
+			loadModel(v, a)
+		end
+	end
+end
+
+local function assignButtons(a)
+	if a.metal then
+		print("VRJuggLua_Model_Loader: Loading METaL Buttons")
+		a.dragBtn = gadget.DigitalInterface("WMButtonB")
+		a.nextBtn = gadget.DigitalInterface("WMButtonRight")
+		a.prevBtn = gadget.DigitalInterface("WMButtonLeft")
+		a.resetBtn = gadget.DigitalInterface("WMButton1")
+		--for scaling action frame at bottom
+		a.increaseBtn = gadget.DigitalInterface("WMButtonPlus")
+		a.decreaseBtn = gadget.DigitalInterface("WMButtonMinus")
+		a.global_pos = {2, 1.5, 1.5}
+	else
+		print("VRJuggLua_Model_Loader: Loading Workstation Buttons")
+		a.nextBtn = gadget.DigitalInterface("VJButton0")
+		a.dragBtn = gadget.DigitalInterface("VJButton2")
+		a.prevBtn = gadget.DigitalInterface("VJButton1")
+		-- for scaling action frame at bottom - no more buttons!
+		a.increaseBtn = nil
+		a.decreaseBtn = nil
+		a.global_pos = {1, 1.5, .25}
+	end
+end
+
+local function addScalingFrameAction(a)
+	-- only gets added if buttons are defined
+	if a.increaseBtn and a.decreaseBtn then
+		Actions.addFrameAction(
+			function()
+				while true do
+					if a.increaseBtn.justPressed then
+						for _, v in ipairs(a.models) do
+							scaleTransform(v, v:getScale():x() * 1.05)
+						end
+					end
+					if a.decreaseBtn.justPressed then
+						for _, v in ipairs(a.models) do
+							scaleTransform(v, v:getScale():x() * 0.95)
+						end
+					end
+					Actions.waitForRedraw()
+				end
+			end
+		)
+	end
+end
+
+function modelLoader(a)
+	if a.manipulate then
+		--load SimSparta for createManipulatableObject() and SimSparta()
+		runfile([[../libraries/SimSparta.lua]])
+	end
+	assignButtons(a)
+	a.models = {}
+	a.parent = a.parent or Transform{}
+	a.scale = a.scale or 1
+	a.position = a.position or {0, 0, 0}
+	local firstArg = arg[1]
+	if string.find(firstArg, ".txt") then
+		a.filename = firstArg
+		loadModelFilesFromTxtFile(a)
+	else
+		loadIndividualModelFiles(a)
+	end
+	if a.factory then
+		local factory = runfile([[../libraries/loadBasicFactory.lua]])
+		a.parent:addChild(factory)
+	end
+	a.parent = CenterTransformAtPosition(a.parent, a.position)
+	if a.parent:getNumChildren() > 0 then
+		RelativeTo.World:addChild(a.parent)
+	end
+	if a.manipulate then
+		SimSparta(a.dragBtn, a.nextBtn, a.prevBtn, a.resetBtn)
+	end
+	if a.outToIVE and a.outPath then
+		osgLua.saveObjectFile(a.parent, a.outPath)
+	end
+	addScalingFrameAction(a)
+end
--- scripts/main_script_metal.lua
+++ scripts/main_script_metal.lua
@@ -2,150 +2,17 @@ require "AddAppDirectory"
 AddAppDirectory()
 
 --load useful_tools for getRandomColor(), changeTransformColor(), and scaleTransform()
-runfile[[../libraries/useful_tools.lua]]
---load SimSparta for createManipulatableObject() and SimSparta() - frame action call
-runfile([[../libraries/SimSparta.lua]])
---add factory model
-runfile([[../libraries/loadBasicFactory.lua]])
--- RelativeTo.World:addChild(factory)
---add lighting to scene
-runfile([[../libraries/simpleLights.lua]])
+runfile[[main_base.lua]]
 
-local models = {}
-
-manipulate = false
-useMETaLButtons = true
-
-if useMETaLButtons then
-	print("VRJuggLua_Model_Loader: Loading METaL Buttons")
-	dragBtn = gadget.DigitalInterface("WMButtonB")
-	nextBtn = gadget.DigitalInterface("WMButtonRight")
-	prevBtn = gadget.DigitalInterface("WMButtonLeft")
-	resetBtn = gadget.DigitalInterface("WMButton1")
-	--for scaling action frame at bottom
-	increaseBtn = gadget.DigitalInterface("WMButtonPlus")
-	decreaseBtn = gadget.DigitalInterface("WMButtonMinus")
-	global_pos = {2, 1.5, 1.5}
-else
-	print("VRJuggLua_Model_Loader: Loading Workstation Buttons")
-	nextBtn = gadget.DigitalInterface("VJButton0")
-	dragBtn = gadget.DigitalInterface("VJButton2")
-	prevBtn = gadget.DigitalInterface("VJButton1")
-	-- for scaling action frame at bottom - no more buttons!
-	increaseBtn = nil
-	decreaseBtn = nil
-	global_pos = {1, 1.5, .25}
-end
-
-local function CenterTransformAtPosition(name,pos)
-    local bound = name:getBound()
-    return Transform{
-        position = -bound:center()+Vec(unpack(pos));
-        name
-    }
-end
-
-local function isAcceptableFileType(file)
-	if string.find(file, ".txt") then
-		return true
-	elseif string.find(file, ".osg") then
-		return true
-	elseif string.find(file, ".ive") then
-		return true	
-	elseif string.find(file, ".obj") then
-		return true
-	elseif string.find(file, ".3ds") then
-		return true
-	else
-		return false
-	end
-end
-
-local function trim(s)
-  return (s:gsub("^%s*(.-)%s*$", "%1"))
-end
-
-local loadModelPathsFromFile = function(a)
-	local parent = Transform{}
-	local my_scale = a.scale or 1
-	local my_position = a.position or global_pos
-	local file = assert(io.open(a.filename, "r"))
-	for v in file:lines() do
-		v = trim(v)
-		if isAcceptableFileType(v) then
-			if manipulate then
-				model = Transform{scale = my_scale, Model(v)}
-				model_xform = Transform{position = my_position, model}
-				table.insert(models, model)
-				changeTransformColor(model_xform, getRandomColor())
-				parent:addChild(createManipulatableObject(model_xform))
-			else
-				print(v)
-				model = Transform{position = my_position, scale = my_scale, Model(v)}
-				changeTransformColor(model, getRandomColor())
-				parent:addChild(model)
-			end
-		end
-	end
-	if parent:getNumChildren() > 0 then
-		RelativeTo.World:addChild(CenterTransformAtPosition(parent,my_position))
-	end
-end
-
-local loadModels = function(a)
-	local parent = Transform{}
-	local my_scale = a.scale or 1
-	local my_position = a.position or global_pos
-	for i, v in pairs(arg) do
-		if string.find(v, ".txt") then
-			loadModelPathsFromFile({filename = v})
-		else
-			if isAcceptableFileType(v) then
-				if manipulate then
-					model = Transform{scale = my_scale, Model(v)}
-					model_xform = Transform{position = my_position, model}
-					table.insert(models, model)
-					changeTransformColor(model_xform, getRandomColor())
-					parent:addChild(createManipulatableObject(model_xform))
-				else
-					model = Transform{position = my_position, scale = my_scale, Model(v)}
-					changeTransformColor(model, getRandomColor())
-					parent:addChild(model)
-				end
-			end
-		end
-	end
-	if parent:getNumChildren() > 0 then
-		RelativeTo.World:addChild(CenterTransformAtPosition(parent,my_position))
-	end
-	if manipulate then
-		SimSparta(dragBtn, nextBtn, prevBtn, resetBtn)
-	end
-end
-
-local function addScalingFrameAction()
-	-- only gets added if buttons are defined
-	if increaseBtn ~= nil and decreaseBtn ~= nil then
-		Actions.addFrameAction(
-			function()
-				while true do
-					if increaseBtn.justPressed then
-						for _, v in ipairs(models) do
-							scaleTransform(v, v:getScale():x() * 1.05)
-						end
-					end
-					if decreaseBtn.justPressed then
-						for _, v in ipairs(models) do
-							scaleTransform(v, v:getScale():x() * 0.95)
-						end
-					end
-					Actions.waitForRedraw()
-				end
-			end
-		)
-	end
-end
-addScalingFrameAction()
-
-loadModels({parent = nil, scale = 1, position = nil})
+modelLoader{
+	factory = false,
+	metal = true,
+	manipulate = true,
+	parent = nil,
+	scale = 1,
+	position = nil,
+	hasColor = false,
+	outToIVE = false,
+	outPath = nil -- must be defined if outToIVE is true
+}
 
