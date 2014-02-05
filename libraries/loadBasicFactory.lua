a47db399e48ab38863153dc0824778fc9cba482c
Leif Berg <lpberg@iastate.edu>
2014-02-05 13:05:57 -0600

    modified factory asset in libraries dir

--- libraries/loadBasicFactory.lua
+++ libraries/loadBasicFactory.lua
@@ -1,9 +1,11 @@
 require("getScriptFilename")
 vrjLua.appendToModelSearchPath(getScriptFilename())
 
-factory = Transform{
+local factory = Transform{
 	position = {0,0,5},
 	orientation = AngleAxis(Degrees(-90), Axis{1.0, 0.0, 0.0}),
 	scale = ScaleFrom.inches,
 	Model(vrjLua.findInModelSearchPath([[../models/basicfactory.ive]]))
 }
+
+return factory
