e08e5b120f4b63f6190d0923bfe4861a9909e054
Leif Berg <lpberg@iastate.edu>
2014-02-12 11:55:04 -0600

    updated mono metal manip launcher to include required parameter

--- drop_directory_here_METaL_manip_mono.bat
+++ drop_directory_here_METaL_manip_mono.bat
@@ -4,5 +4,5 @@ for /F "tokens=*" %%A in ('dir /b/a-d') do @echo %%~dpnxA && @echo %%~dpnxA >> %
 cd "%~dp0"
 V:\Applications\vrjugglua\windows-snapshot\bin\NavTestbed.exe ^
 s:/jconf30/METaL.tracked.mono.reordered.withwand.jconf S:\jconf30\mixins\METaL.wiimotewandbuttons.jconf ^
-%~dp0\scripts\main_script_METaL_manip.lua  %*
+%~dp0\scripts\main_script_METaL_manip.lua  %~dp0myfile.txt
 DEL %~dp0myfile.txt
