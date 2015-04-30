ca1fb0bc353a1972be860cc98bf1073f5fb45057
Leif Berg <lpberg@iastate.edu>
2015-04-29 20:15:40 -0500

    updated new vrjugglua location and non-reordered JCONFs

--- drop_directory_here_METaL_manip_stereo.bat
+++ drop_directory_here_METaL_manip_stereo.bat
@@ -2,7 +2,7 @@
 cd %1
 for /F "tokens=*" %%A in ('dir /b/a-d') do @echo %%~dpnxA && @echo %%~dpnxA >> %~dp0myfile.txt
 cd "%~dp0"
-V:\Applications\vrjugglua\windows-snapshot\bin\NavTestbed.exe ^
-s:/jconf30/METaL.tracked.stereo.reordered.withwand.jconf S:\jconf30\mixins\METaL.wiimotewandbuttons.jconf ^
+C:\Users\Public\Documents\DEMOS\windows-snapshot\bin\NavTestbed.exe ^
+C:/Users/Public/Documents/jconf30/METaL.tracked.stereo.withwand.jconf C:/Users/Public/Documents/jconf30/mixins/METaL.wiimotewandbuttons.jconf ^
 %~dp0\scripts\main_script_METaL_manip.lua %~dp0myfile.txt
 DEL %~dp0myfile.txt
